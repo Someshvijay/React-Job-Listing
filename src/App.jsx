1971ed68d5991e336446eb88c8647d74c0c09174
Somesh <someshvijay.m3110@gmail.com>
2024-06-28 22:01:52 +0530

    fetching data using server

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} fr
 import Homepage from './pages/Homepage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
+import JobPage from './pages/JobPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 
@@ -13,6 +14,7 @@ const router = createBrowserRouter(
   <Route path='/' element={<MainLayout />}>
     <Route index element = {<Homepage/>}/>
     <Route path='/jobs' element = {<JobsPage/>}/>
+    <Route path='/jobs/:id' element = {<JobPage/>}/>
     <Route path='*' element = {<NotFoundPage/>}/>
   </Route>
   )
