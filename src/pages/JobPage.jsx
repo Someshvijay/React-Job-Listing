6ebae170e03a505d8e4b9a95ca209123531081d0
Somesh <someshvijay.m3110@gmail.com>
2024-06-29 16:39:50 +0530

    Added CRUD functionalities

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,13 +1,25 @@
 import { FaMapMarker } from 'react-icons/fa'
 import { FaArrowLeft } from 'react-icons/fa6'
-import { useLoaderData, useParams } from 'react-router-dom'
+import { useLoaderData, useNavigate, useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 
-const JobPage = () => {
+const JobPage = ({deleteJob}) => {
 
     const { id } = useParams()
     const job = useLoaderData()
+    const navigate = useNavigate()
+    
+    const onDeleteClick = (JobId) => {
+      const confirm = window.confirm('Are you sure you want to delete this listing ?')
+      if (!confirm) return
+
+      deleteJob(JobId);
+      toast.success("Job Deleted Successfully")
+      navigate('/jobs')
+
+    }
     
   return (
     
@@ -86,11 +98,12 @@ const JobPage = () => {
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
               <Link
-                to={`/jobs/edit/${job.id}`}
+                to={`/edit-job/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link>
               <button
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                onClick={ () => onDeleteClick(job.id) }
               >
                 Delete Job
               </button>
